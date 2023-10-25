56949fb172c4066d51090e5a782fae2d4d7ad45a
Shaw <sunxycn98@gmail.com>
2023-10-24 23:25:09 -0400

    modified upload script

--- upload.sh
+++ upload.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
-if [ $# -eq 0 ]; then
-  echo "Usage: $0 <uf2_file>"
-  exit 1
+EXPECTED_ARGS=2
+E_BADARGS=65
+
+if [ $# -ne 2 ]
+then
+  echo "Usage: $0 [load | run | flash | disable] <uf2_file>"
+  exit $E_BADARGS
 fi
 
-UF2_FILE=$1
+OPERATION=$1
+UF2_FILE=$2
 
 # GPIO Numbers, 
 # on Jetson, pin 7 is gpio216 and pin 11 is gpio50
@@ -30,27 +35,64 @@ sleep 0.1
 echo out > /sys/class/gpio/gpio$BTLD_PIN/direction
 echo out > /sys/class/gpio/gpio$RUN_PIN/direction
 sleep 0.1
-echo 1 > /sys/class/gpio/gpio$BTLD_PIN/value
-echo 1 > /sys/class/gpio/gpio$RUN_PIN/value
-sleep 0.1
 
-# Toggle to bootloader
-echo 0 > /sys/class/gpio/gpio$RUN_PIN/value
-sleep 0.1
-echo 0 > /sys/class/gpio/gpio$BTLD_PIN/value
-sleep 0.5
-echo 1 > /sys/class/gpio/gpio$RUN_PIN/value
-sleep 1
-
-# Upload code
-sudo picotool load $UF2_FILE
-sleep 0.5
-echo 1 > /sys/class/gpio/gpio$BTLD_PIN/value
-sleep 0.5
-sudo picotool reboot
+case "$OPERATION" in
+    "load"|"run"|"flash"|"disable")
+        # notice that for all cases, we consdier the RUN_PIN is high
+        # which set by services/mbot_start_networking.py 
+        case "$OPERATION" in
+            "load")
+                echo "Loading action for $UF2_FILE..."
+                echo 0 > /sys/class/gpio/gpio$RUN_PIN/value
+                sleep 0.1
+                echo 0 > /sys/class/gpio/gpio$BTLD_PIN/value
+                sleep 0.5
+                echo 1 > /sys/class/gpio/gpio$RUN_PIN/value
+                sleep 0.5
+                sudo picotool load $UF2_FILE
+                sleep 0.5
+                ;;
+            "run")
+                echo "Running action for $UF2_FILE..."
+                echo 0 > /sys/class/gpio/gpio$RUN_PIN/value
+                sleep 0.1
+                echo 1 > /sys/class/gpio/gpio$BTLD_PIN/value
+                sleep 0.5
+                echo 1 > /sys/class/gpio/gpio$RUN_PIN/value
+                sleep 1
+                ;;
+            "flash")
+                echo "Flashing action for $UF2_FILE..."
+                echo 0 > /sys/class/gpio/gpio$RUN_PIN/value
+                sleep 0.1
+                echo 0 > /sys/class/gpio/gpio$BTLD_PIN/value
+                sleep 0.5
+                echo 1 > /sys/class/gpio/gpio$RUN_PIN/value
+                sleep 0.5
+                sudo picotool load $UF2_FILE
+                sleep 0.5
+                echo 1 > /sys/class/gpio/gpio$BTLD_PIN/value
+                sleep 0.5
+                sudo picotool reboot
+                ;;
+            "disable")
+                echo "Disable action for $UF2_FILE..."    
+                echo 0 > /sys/class/gpio/gpio$BTLD_PIN/value
+                sleep 0.5
+                echo 0 > /sys/class/gpio/gpio$RUN_PIN/value
+                sleep 1
+                ;;
+        esac
+        ;;
+    *)
+        echo "'$OPERATION' is not a valid operation. Please provide one of the following operations: load, run, flash, disable"
+        exit 1
+        ;;
+esac
 
 # Make pins High-Z and unexport
 echo in > /sys/class/gpio/gpio$BTLD_PIN/direction
 echo in > /sys/class/gpio/gpio$RUN_PIN/direction
 echo $BTLD_PIN > /sys/class/gpio/unexport
 echo $RUN_PIN > /sys/class/gpio/unexport
+
